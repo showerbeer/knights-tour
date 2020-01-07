7160d24d0cb3bf55e7cb1f66f3b41856527897ff
Christian Lassen <christian@mudbath.com.au>
2020-01-07 23:06:48 +1100

    Initial commit. Basic MVP working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { arrayIncludes, getLegalMoves, isLight } from './utils';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const STARTING_SQUARE = 2;
+const board = [];
+
+for (let i = 0; i < 64; i++) {
+  board.push({
+    colour: isLight(i) ? 'white' : 'black',
+    visited: i === STARTING_SQUARE,
+    currentSquare: i === STARTING_SQUARE,
+    squareNumber: i,
+    highlighted: false
+  });
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentKnightPosition: STARTING_SQUARE
+    }
+  }
+
+  handleSquareClick = (isLegalMove, square) => () => {
+    if (isLegalMove) {
+      board[square].visited = true;
+      this.setState({
+        currentKnightPosition: square
+      });
+    }
+  }
+
+  render() {
+    let knightPos = this.state.currentKnightPosition;
+    const legalMoves = getLegalMoves(knightPos);
+    return <div className="App">
+      <div className="chessboard">
+        {board.map(square => {
+          const isLegalMove = !square.visited && arrayIncludes(legalMoves, square.squareNumber);
+          const className = `${square.colour} ${isLegalMove ? 'potential' : ''} ${square.visited ? 'visited' : ''}`
+          return (
+            <div
+              onClick={this.handleSquareClick(isLegalMove, square.squareNumber)}
+              key={square.squareNumber}
+              className={className}
+            >
+              {square.squareNumber === knightPos && <span>&#9822;</span>}
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,20 @@
+export const arrayIncludes = (arr, val) => {
+    return arr.indexOf(val) >= 0;
+};
+
+export const isLight = (num) => {
+    return 0 === (((num >> 3) ^ num) & 1);
+}
+
+export const squareDist = (a, b) => {
+    let x1 = a & 7,
+        x2 = b & 7;
+    let y1 = a >> 3,
+        y2 = b >> 3;
+
+    return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
+}
+
+export const getLegalMoves = (s) => {
+    return [s - 6, s - 10, s - 15, s - 17, s + 6, s + 10, s + 15, s + 17].filter(x => x >= 0 && x < 64 && squareDist(s, x) <= 2);
+}
